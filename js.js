0ce3a414a7b2f6df4c3d562972125630672418b8
Evgenii Tolstykh <prostojeka15@gmail.com>
2022-05-01 23:53:45 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,9 @@
+"use sctrict"
+
+// alert('hello')
+
+// const result = confirm('are you here');
+// console.log(result);
+
+const answer = prompt("Вам есть 18?" , "18");
+console.log(typeof(answer));
