f16f404fdc35adc7c4ec3f3a7fecda6bd3f24574
Evgenii Tolstykh <prostojeka15@gmail.com>
2022-05-13 17:42:36 +0200

    цыкл и цыкл в цыкле

--- script.js
+++ script.js
@@ -42,3 +42,115 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for(let j = 0; j < 3; j++){
+        console.log(j);
+    }
+
+
+}
+
+// function firstTask() {
+//     // Пишем решение вот тут
+//     let i = 5;
+//     while (i< 10){
+//     console.log (i);
+//     i++;
+// }
+// }
+
+function firstTask (){
+    for(let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+
+function secondTask() {
+
+    for (i = 20; i > 9; i--) {
+        console.log(i);
+    }
+}
+ 
+
+function thirdTask() {
+    for(let i = 2; i < 12; i +=2) {
+        console.log(i);
+     }  
+}
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+// Что за function такой мы узнаем совсем скоро :) Это нужно для автоматических тестов
+// Пока нас интересуют циклы и условия
+
+function firstTask() {
+    for (let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+firstTask()
+
+function secondTask() {
+    for (let i = 20; i >= 10; i--) {
+        if (i === 13) break;
+        console.log(i)
+    }
+}
+
+secondTask()
+
+function thirdTask() {
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+thirdTask()
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    let i = 2;
+
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+fourthTask()
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
